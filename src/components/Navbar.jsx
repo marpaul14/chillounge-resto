98adc0bf7574a9ca7bb3fad2cbaf168520aad576
marpaul <marpauljaybunani1314@gmail.com>
2023-10-31 12:21:29 +0800

    added small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,16 @@ function Navbar() {
     <div className="bg-[#01022b] text-slate-100 w-full h-[3.5rem] mx-auto ">
       <div className="flex justify-between items-center py-4 px-6">
         <div>
-          <h1 className="font-semibold uppercase text-xl">Chillounge 😋</h1>
+          <h1 className="font-semibold uppercase text-xl hover:scale-105 duration-300 ease-in-out">
+            Chillounge
+          </h1>
         </div>
         <div className="">
           <ul className="flex px-2 space-x-4 uppercase">
             <li>Home</li>
             <li>Order</li>
             <li>Reservation</li>
+            <li>Contact</li>
           </ul>
         </div>
       </div>
