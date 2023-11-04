66e15dbb013c51a9a19e68f097334fc84bc3ef52
marpaul <marpauljaybunani1314@gmail.com>
2023-11-04 21:24:11 +0800

    need to change leisure img to fit large view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="bg-[#01022b] text-slate-100  w-full mx-auto px-4">
       <div className="px-5 py-6 grid grid-cols-2 w-full ">
         <div className="py-4 pr-5">
-          <h1 className="font-semibold text-2xl pb-3 lg:text-4xl">ABOUT US</h1>
+          <h1 className="font-semibold text-2xl pb-3 lg:text-4xl ">ABOUT US</h1>
           <p className="lg:text-2xl">
             Welcome to Chillounge Resto & KTV Bar, your ultimate destination for
             relaxation and entertainment. Our doors are open from 9am to 10pm
--- src/components/Leisure.jsx
+++ src/components/Leisure.jsx
@@ -4,12 +4,12 @@ function Leisure() {
   return (
     <div className="py-1 mx-auto ">
       <div className="grid grid-cols-2 w-full space-x-2 text-[#01022b]">
-        <div>
-          <img src={KaraokeImg} className="rounded-xl" />
-        </div>
         <div className="">
-          <h1>Leisure</h1>
-          <p>
+          <img src={KaraokeImg} className="rounded" />
+        </div>
+        <div className="pt-5 lg:pl-[-10]">
+          <h1 className="text-2xl font-semibold lg:text-4xl">Leisure</h1>
+          <p className="mt-3 lg:text-2xl lg:mt-5">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
