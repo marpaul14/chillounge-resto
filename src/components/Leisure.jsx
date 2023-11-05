89138708deeed3333c32977e8c15e91a6261167c
marpaul <marpauljaybunani1314@gmail.com>
2023-11-05 21:07:13 +0800

    spacing finalized

--- src/components/Leisure.jsx
+++ src/components/Leisure.jsx
@@ -2,7 +2,7 @@ import KaraokeImg from "../assets/karaoke.jpg";
 
 function Leisure() {
   return (
-    <div className="mx-auto cursor-default w-full lg:w-[85%]">
+    <div className="mx-auto cursor-default w-full lg:w-[85%] ">
       <div className="grid grid-cols-2 w-full space-x-2 text-[#01022b]">
         <div className="pt-3">
           <img src={KaraokeImg} className="rounded w-full h-[95%]" />
@@ -16,7 +16,9 @@ function Leisure() {
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
             available for special events, making us the ideal venue for your
-            celebrations.
+            celebrations. Join us at Chillounge for an unforgettable combination
+            of great food, fantastic music, and a warm, welcoming atmosphere.
+            Your memorable moments are just a visit away!
           </p>
         </div>
       </div>
