4f7b564ea8efaf048dae817d9343f6f05c40d54d
marpaul <marpauljaybunani1314@gmail.com>
2023-11-07 22:08:21 +0800

    offers component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Leisure from "./components/Leisure";
 import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Leisure />
+      <Offers />
     </>
   );
 }
---
+++ src/components/Offers.jsx
@@ -0,0 +1,46 @@
+function Offers() {
+  return (
+    <div className="w-full bg-[#01022b] h-[50vh] text-slate-100 py-2">
+      <div className="text-2xl text-center pt-3 py-3">
+        <h1>ChilLounge Offers</h1>
+      </div>
+      <div className="grid grid-cols-3 px-3 space-x-2">
+        <div className="">
+          <h1 className="text-center text-lg border-b">Food & Beverages</h1>
+          <p className="text-center pt-3">
+            For those looking to indulge in some musical fun, we provide
+            well-equipped KTV rooms where you can sing your heart out with
+            friends and family. Additionally, our spacious function hall is
+            available for special events, making us the ideal venue for your
+            celebrations. Join us at Chillounge for an unforgettable combination
+            of great food, fantastic music, and a warm, welcoming atmosphere.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-center text-lg border-b">KTV room</h1>
+          <p className="text-center pt-3">
+            For those looking to indulge in some musical fun, we provide
+            well-equipped KTV rooms where you can sing your heart out with
+            friends and family. Additionally, our spacious function hall is
+            available for special events, making us the ideal venue for your
+            celebrations. Join us at Chillounge for an unforgettable combination
+            of great food, fantastic music, and a warm, welcoming atmosphere.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-center text-lg border-b">Function Hall</h1>
+          <p className="text-center pt-3">
+            For those looking to indulge in some musical fun, we provide
+            well-equipped KTV rooms where you can sing your heart out with
+            friends and family. Additionally, our spacious function hall is
+            available for special events, making us the ideal venue for your
+            celebrations. Join us at Chillounge for an unforgettable combination
+            of great food, fantastic music, and a warm, welcoming atmosphere.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Offers;
