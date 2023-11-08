b47bb503c79284fe057049231349ce509811d31f
marpaul <marpauljaybunani1314@gmail.com>
2023-11-08 19:53:58 +0800

    fix mobile and large view

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,13 +1,13 @@
 function Offers() {
   return (
     <div className="w-full bg-[#01022b] h-[50vh] text-slate-100 py-2">
-      <div className="text-2xl text-center pt-3 py-3">
+      <div className="text-2xl text-center pt-3 py-3 lg:text-4xl">
         <h1>ChilLounge Offers</h1>
       </div>
       <div className="grid grid-cols-3 px-3 space-x-2">
         <div className="">
           <h1 className="text-center text-lg border-b">Food & Beverages</h1>
-          <p className="text-center pt-3">
+          <p className="text-center pt-3 text-base  lg:text-2xl">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
@@ -18,7 +18,7 @@ function Offers() {
         </div>
         <div>
           <h1 className="text-center text-lg border-b">KTV room</h1>
-          <p className="text-center pt-3">
+          <p className="text-center pt-3 text-base  lg:text-2xl">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
@@ -29,7 +29,7 @@ function Offers() {
         </div>
         <div>
           <h1 className="text-center text-lg border-b">Function Hall</h1>
-          <p className="text-center pt-3">
+          <p className="text-center pt-3 text-base  lg:text-2xl">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
