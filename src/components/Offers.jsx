2973b0c7727cd4fe82b61c307757153652a52c77
marpaul <marpauljaybunani1314@gmail.com>
2023-11-08 19:58:17 +0800

    sizing changed

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,13 +1,15 @@
 function Offers() {
   return (
-    <div className="w-full bg-[#01022b] h-[50vh] text-slate-100 py-2">
+    <div className="w-full bg-[#01022b] h-[50vh] text-slate-100 py-2 mx-auto lg:w-[85%]">
       <div className="text-2xl text-center pt-3 py-3 lg:text-4xl">
         <h1>ChilLounge Offers</h1>
       </div>
       <div className="grid grid-cols-3 px-3 space-x-2">
         <div className="">
-          <h1 className="text-center text-lg border-b">Food & Beverages</h1>
-          <p className="text-center pt-3 text-base  lg:text-2xl">
+          <h1 className="text-center text-lg border-b lg:text-2xl">
+            Food & Beverages
+          </h1>
+          <p className="text-center pt-3 text-base lg:text-xl lg:pt-5">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
@@ -17,8 +19,8 @@ function Offers() {
           </p>
         </div>
         <div>
-          <h1 className="text-center text-lg border-b">KTV room</h1>
-          <p className="text-center pt-3 text-base  lg:text-2xl">
+          <h1 className="text-center text-lg border-b lg:text-2xl">KTV room</h1>
+          <p className="text-center pt-3 text-base lg:text-xl lg:pt-5">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
@@ -28,8 +30,10 @@ function Offers() {
           </p>
         </div>
         <div>
-          <h1 className="text-center text-lg border-b">Function Hall</h1>
-          <p className="text-center pt-3 text-base  lg:text-2xl">
+          <h1 className="text-center text-lg border-b lg:text-2xl">
+            Function Hall
+          </h1>
+          <p className="text-center pt-3 text-base lg:text-xl lg:pt-5">
             For those looking to indulge in some musical fun, we provide
             well-equipped KTV rooms where you can sing your heart out with
             friends and family. Additionally, our spacious function hall is
