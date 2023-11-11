c10506d5b7948e5a2b683a7389ba1cfade009c82
marpaul <marpauljaybunani1314@gmail.com>
2023-11-11 19:27:33 +0800

    images display fixed

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -5,18 +5,18 @@ import foodImg4 from "../assets/foodimg4.jpg";
 
 function Foods() {
   return (
-    <div className="w-full h-[50vh] mx-auto py-5">
+    <div className="w-full h-[45vh] mx-auto py-5">
       <div className="flex flex-cols-4 justify-center items-center space-x-1 w-full h-[45vh]">
-        <div className="">
+        <div className="col-span-1 flex flex-col justify-between h-full">
           <img src={foodImg1} className="w-[200px] h-[300px] " />
         </div>
-        <div className="">
+        <div className="col-span-1">
           <img src={foodImg2} className="w-[200px] h-[300px]" />
         </div>
-        <div className="">
+        <div className="col-span-1 flex flex-col justify-between h-full">
           <img src={foodImg3} className="w-[200px] h-[300px]" />
         </div>
-        <div className="">
+        <div className="col-span-1">
           <img src={foodImg4} className="w-[200px] h-[300px]" />
         </div>
       </div>
