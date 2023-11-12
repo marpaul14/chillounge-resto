3e56de7e160a3de175007d13a8833098e16962a3
marpaul <marpauljaybunani1314@gmail.com>
2023-11-12 21:44:04 +0800

    sidebar component added

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
