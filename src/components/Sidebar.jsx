39d952d3a308265b0bbf8dc537ca2e5e6c2e66ae
marpaul <marpauljaybunani1314@gmail.com>
2023-11-13 20:54:25 +0800

    styled sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,11 +2,11 @@ function Sidebar() {
   return (
     <div className="w-full h-auto text-slate-100 mx-auto">
       <div className="relative">
-        <ul>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
+        <ul className="">
+          <li className="">Home</li>
+          <li className="">Order</li>
+          <li className="">Reservation</li>
+          <li className="">Contact</li>
         </ul>
       </div>
     </div>
