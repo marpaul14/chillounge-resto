bf59227b36118468267275446c22f4740be2796b
marpaul <marpauljaybunani1314@gmail.com>
2023-11-28 19:37:02 +0800

    fixing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { BrowserRouter, NavLink, Routes } from "react-router-dom";
-import About from "./components/About";
-import Foods from "./components/Foods";
-import Hero from "./components/Hero";
-import Leisure from "./components/Leisure";
-import Navbar from "./components/Navbar";
-import Offers from "./components/Offers";
+
+import AppLayout from "./components/AppLayout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <NavLink to={<Navbar />} />
-          <Hero />
-          <About />
-          <Leisure />
-          <Offers />
-          <Foods />
+          <AppLayout />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Leisure from "./Leisure";
+import Navbar from "./Navbar";
+import Navbar from "./Navbar";
+
+function AppLayout() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Leisure />
+      <Offers />
+      <Foods />
+    </div>
+  );
+}
+
+export default AppLayout;
