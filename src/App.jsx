4b9227c1dfc591a9b787aaa1f60e33d957662849
marpaul <marpauljaybunani1314@gmail.com>
2023-12-03 22:52:06 +0800

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AppLayout from "./pages/AppLayout";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import MoreDetails from "./pages/MoreDetails";
+import Order from "./pages/Order";
+import Reservation from "./pages/Reservation";
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<AppLayout />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="home" element={<Home />} />
+          <Route path="more-details" element={<MoreDetails />} />
+          <Route path="order" element={<Order />} />
+          <Route path="Reservation" element={<Reservation />} />
         </Routes>
       </BrowserRouter>
     </>
